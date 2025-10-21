5a01c941376f7d02dc05d9e96a57cebf0cfae620
unknown <geronimo.martinez-n@labinfo.is.escuelaing.edu.co>
2025-10-21 09:13:02 -0500

    implementando proxy

---
+++ proxy-service/src/main/java/edu/eci/escuelaing/proxy_service/controller/ProxyServiceController.java
@@ -0,0 +1,70 @@
+package edu.eci.escuelaing.proxy_service.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+
+@RestController
+@RequestMapping("/")
+public class ProxyServiceController {
+    private static final String USER_AGENT = "Mozilla/5.0";
+
+    @GetMapping
+    public String manageRequest(@RequestParam String path) {
+        try {
+            return request(path);
+        } catch (IOException e) {
+            return "{\"error\":\"La peticion fallo\"}";
+        }
+    }
+
+    public String request(String path) throws IOException {
+        String url;
+        if (path.startsWith("mathService1")) {
+            url = "http://localhost:8081" + path;
+        } else if (path.startsWith("mathService2")) {
+            url = "http://localhost:8082" + path;
+        } else {
+            return "{\"error\":\"Ruta no válida\"}";
+        }
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            return response.toString();
+        } else {
+            return "{\"error\":\"La peticion fallo\"}";
+        }
+    }
+}
+
+
+
+
+
+
